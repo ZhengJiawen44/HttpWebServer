1106579a84280f5db5ba4580cc9bab4cf270030e
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-11-24 18:22:30 +0800

    added integration test for httpServer

---
+++ tests/HttpServer.test.ts
@@ -0,0 +1,26 @@
+import Server from "../HttpServer";
+import * as net from "net";
+
+test("server is listening on designated port", (done) => {
+  const httpServer = new Server(() => { });
+
+  httpServer.listen(45678, "localhost", () => {
+    expect(httpServer.TCPServer).toBeInstanceOf(net.Server);
+    httpServer.close();
+    done();
+  });
+});
+
+test("server has correct <maxQuota> attrbutes", () => {
+  const httpServer = new Server(() => { }, { requestQuota: 2 });
+  expect(httpServer.requestQuota).toBe(2);
+})
+
+test("server has correct <timeout> attrbutes", () => {
+  const httpServer = new Server(() => { }, { timeout: 10 });
+  expect(httpServer.timeout).toBe(10);
+})
+
+
+
+
