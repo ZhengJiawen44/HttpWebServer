06078fc7985596c0cd255ce748d2c18f4e489e71
Zheng Jiawen <zhengjiawen44@gmail.com>
2025-11-27 22:40:17 +0800

    feat: added content compression support for: gzip, deflate, br, and zstd

---
+++ src/utils/compressResponse.ts
@@ -0,0 +1,40 @@
+import { Writable, Readable } from "stream";
+import { createGzip, createDeflate, createBrotliCompress, createZstdCompress } from "zlib";
+import { pipeline } from "stream/promises";
+
+export async function compressResponse(response: string | Buffer, compressionEncodings: string[]): Promise<[Buffer, string]> {
+
+  let chosenEncoding = null;
+  let buffers = [];
+  const readableStream = Readable.from(response);
+  const writeableStream = new Writable({
+    write(chunk, encoding, callback) {
+      buffers.push(chunk);
+      callback();
+    }
+  });
+
+  for (let encoding of compressionEncodings) {
+    switch (encoding) {
+      case "deflate":
+        chosenEncoding = "deflate"
+        await pipeline(readableStream, createDeflate(), writeableStream);
+        break;
+      case "gzip":
+        chosenEncoding = "gzip";
+        await pipeline(readableStream, createGzip(), writeableStream);
+        break;
+      case "br":
+        chosenEncoding = "br";
+        await pipeline(readableStream, createBrotliCompress(), writeableStream);
+        break;
+      case "zstd":
+        chosenEncoding = "zstd";
+        await pipeline(readableStream, createZstdCompress(), writeableStream);
+        break;
+    }
+    if (chosenEncoding) break;
+  }
+  return [Buffer.concat(buffers), chosenEncoding];
+}
+
--- src/utils/url.ts
+++
@@ -1,4 +0,0 @@
-
-export function parse(url: string) {
-
-}
